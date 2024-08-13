108a2b04dc252f0993990b9e0b1533602d1f46d0
15217613 <15217613@uagro.mx>
2024-08-13 17:16:56 -0600

    MODIFIED: Agregadas rutas para web

--- src/Http/Controllers/WEB/KingMonitorController.php
+++ src/Http/Controllers/WEB/KingMonitorController.php
@@ -2,42 +2,36 @@
 
 namespace ByCarmona141\KingMonitor\Http\Controllers\WEB;
 
+use ByCarmona141\KingMonitor\Facades\KingMonitor;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
-
-use ByCarmona141\KingMonitor\Facades\KingMonitor;
 
 //use ByCarmona141\KingMonitor\Models\KingMonitor;
 
 class KingMonitorController extends Controller {
-    public function index() {
-        return view('king-monitor::monitor');
+    public function index(Request $request) {
+        // Obtenemos las estadisticas del monitor
+        $statistics = KingMonitor::statistics();
+        $historical = KingMonitor::historical();
+
+        // Convertimos el json en arreglo
+        $statistics = $statistics->original;
+        $historical = $historical->original;
+
+        // Mandamos los datos a la vista
+        return view('king-monitor::monitor')->with('statistics', $statistics)->with('historical', $historical);
     }
-//    public function index(Request $request) {
-//        // Obtenemos las estadisticas del monitor
-//        $kingMonitor = new KingMonitor();
-//        $statistics = $kingMonitor->statistics();
-//        $historical = $kingMonitor->historical();
-//
-//        // Convertimos el json en arreglo
-//        $statistics = $statistics->original;
-//        $historical = $historical->original;
-//
-//        // Mandamos los datos a la vista
-//        return view('king.king-monitor')->with('statistics', $statistics)->with('historical', $historical);
-//    }
-//
-//    public function show($king_user_id) {
-//        // Obtenemos las estadisticas del monitor
-//        $kingMonitor = new KingMonitor();
-//        $statistics = $kingMonitor->statisticsUser($king_user_id);
-//        $historical = $kingMonitor->userHistorical($king_user_id);
-//
-//        // Convertimos el json en arreglo
-//        $statistics = $statistics->original;
-//        $historical = $historical->original;
-//
-//        // Mandamos los datos a la vista
-//        return view('king.user')->with('statistics', $statistics)->with('historical', $historical);
-//    }
+
+    public function show($king_user_id) {
+        // Obtenemos las estadisticas del monitor
+        $statistics = KingMonitor::userStatistics($king_user_id);
+        $historical = KingMonitor::userHistorical($king_user_id);
+
+        // Convertimos el json en arreglo
+        $statistics = $statistics->original;
+        $historical = $historical->original;
+
+        // Mandamos los datos a la vista
+        return view('king-monitor::user')->with('statistics', $statistics)->with('historical', $historical);
+    }
 }
