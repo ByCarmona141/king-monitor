f5a3bc9fd5b3512e79c7275c65d73a7f6f4dfcb7
15217613 <15217613@uagro.mx>
2024-08-19 13:46:41 -0600

    MODIFIED: modified view

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,16 +2,14 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
         <title>
-            @if (isset($title))
-                {{$title}}
-            @else
-                {{ config('app.name', 'Laravel') }}
-            @endif
+            @yield('title')
         </title>
+
+        <script src="https://cdn.tailwindcss.com"></script>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
@@ -22,26 +20,18 @@
     </head>
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
-            @include('layouts.navigation')
+{{--            @include('king-monitor::layouts.navigation')--}}
 
             <!-- Page Heading -->
-            @if (isset($header))
-                <header class="bg-white dark:bg-gray-800 shadow">
-                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                        {{ $header }}
-                    </div>
-                </header>
-            @endif
+            @yield('header')
 
             <!-- Page Content -->
             <main>
-                {{ $slot }}
+                @yield('content')
             </main>
         </div>
 
         {{-- script --}}
-        @if (isset($script))
-            {{ $script }}
-        @endif
+        @yield('script')
     </body>
 </html>
