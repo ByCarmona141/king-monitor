83bb92ce713222c7ddf5cdfcd52f3556f113711c
15217613 <15217613@uagro.mx>
2024-09-17 19:18:45 -0600

    FIXED: fixed route name error

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,5 @@
 use Illuminate\Support\Facades\Route;
 use ByCarmona141\KingMonitor\Http\Controllers\WEB\KingMonitorController;
 
-Route::get('/monitor/{king_user_id}', [KingMonitorController::class, 'show'])->name('monitor');
-Route::get('/monitor', [KingMonitorController::class, 'index'])->name('monitor');
+Route::get('/monitor/{king_user_id}', [KingMonitorController::class, 'show'])->name('monitor.show');
+Route::get('/monitor', [KingMonitorController::class, 'index'])->name('monitor.index');
